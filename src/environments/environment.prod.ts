3848009f9bde2ec0fac23c79689086a604dd3c1f
pavelray <pavelray@gmail.com>
2019-03-08 09:37:00 +0530

    updated github deployment instruction

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: 'AIzaSyA8AIn2fI1BfFs4L--JUte5yVyj3O5GPco',
+    authDomain: 'fir-demo-8cac8.firebaseapp.com',
+    databaseURL: 'https://fir-demo-8cac8.firebaseio.com',
+    projectId: 'fir-demo-8cac8',
+    storageBucket: 'fir-demo-8cac8.appspot.com',
+    messagingSenderId: '688072657527'
+  }
 };
